2e4642996b4c433b8926a47858cea530de638a88
ragvaldus <90042874+ragvaldus@users.noreply.github.com>
2024-06-12 13:33:46 +0200

    Handles matrix now. The diagonal of the final matrix is zero.

--- src/FFDIAG.jl
+++ src/FFDIAG.jl
@@ -22,7 +22,7 @@ function ffdiag(Cs)
     V = I
     n = 1
     
-    Ds = zeros(dim1, dim2, length(Carr))
+    Ds = zeros(dim1, length(Carr))
     Es = zeros(dim1, dim2, length(Carr))
 
     # doiesnt do anything right now because V = I
@@ -34,26 +34,26 @@ function ffdiag(Cs)
     for run in 1:runs
         
         # fill the Ds and Es matrices
-        for i in eachindex(Carr)
-            Ds[:,:,i] = diagm(diag(Carr[i]))
-            Es[:,:,i] = Carr[i]
+        for k in eachindex(Carr)
+            Ds[:,k] = diag(Carr[k])
+            Es[:,:,k] = Carr[k]
 
             # make the diagonal of Es zero
             for j in 1:dim1
-                Es[j,j,i] = 0                
+                Es[j,j,k] = 0                
                 end
-
-            
+        
         # calculate W (17) 
         z = zeros(dim1, dim2)
         y = zeros(dim1, dim2)
         
         # Todo: Implement broadcast for the last loop sum(Ds[:,i] .* Ds[:,j] 
-        for i in 1:dim1-1
-            for j in 1+1:dim2
-                for k in 1:length(Carr)
-                    z[i, j] += Ds[k, i] * Ds[k, j]
-                    y[i, j] += Ds[k, j] * Es[k, i, j] # This is the same as += 0.5* Ds[k,j]*(Es[k,i,j] + Es[k,j,i]) as according to the paper
+        for i in 1:dim1
+            for j in 1:dim2
+                for k in eachindex(Carr)
+                    z[i, j] += Ds[i, k] * Ds[i, k]
+                    y[i, j] += 0.5* Ds[j , k]*(Es[i,j,k] + Es[j,i,k]) 
+                    # y[i, j] += Ds[j, k] * Es[i, j, k] # This is the same as += 0.5* Ds[k,j]*(Es[k,i,j] + Es[k,j,i]) as according to the paper
                 end
             end
         end
@@ -62,8 +62,15 @@ function ffdiag(Cs)
         # put the broadcast directly in the loop and elimnate the above 3 for loops
         for i in 1:dim1
             for j in i:dim2
-                W[i, j] = (z[i, j]*y[i, j]-y[i, i]*z[i, j])/(z[j,j]*z[i,i]-z[i,j]^2)
-                W[j, i] = (z[i,j]*y[i,j]-z[j,j]*y[j,i])/(z[j,j]*z[i,i]-z[i,j]^2)
+                # This was put in place so we dont divide by zero
+                # Todo: Investigate why zero even shows up
+                if z[j,j]*z[i,i]-z[i,j]^2 != 0
+                    W[i, j] = (z[i, j]*y[j, i]-y[i, i]*z[i, j])/(z[j,j]*z[i,i]-z[i,j]^2)
+                    W[j, i] = (z[i,j]*y[i,j]-z[j,j]*y[j,i])/(z[j,j]*z[i,i]-z[i,j]^2)
+                else
+                    W[i, j] = 0
+                    W[j, i] = 0
+                end
             end
         end
 
@@ -75,9 +82,12 @@ function ffdiag(Cs)
         end
 
         V = (I + W) * V
-        for C in Carr
-            C = (I + W) * C * transpose((I + W))
+
+        @info(W)
+        for K in eachindex(Carr)
+            Carr[K] = (I + W[k]) * Carr[k] * (I + W[k])'
         end 
+
         n += 1
 
     end
--- test/FFDiag_tests.jl
+++ test/FFDiag_tests.jl
@@ -1,6 +1,7 @@
 using AJD
 using Test
 using MatrixDepot
+using Plots
 # include("../src/FFDIAG.jl")
 # using .FFDIAG: ffdiag
 
@@ -9,12 +10,19 @@ import AJD: ffdiag
 @testset "AJD.jl" begin
     # Write your tests here.
     # generate 5 random matrices
-    matrix = [matrixdepot("minij", 5) for _ in 1:2]
-    # matrix = matrixdepot("minij", 5)s<
+    matrix = [matrixdepot("minij", 5) for i in 1:2]
+    plot(matrix[1])
+    # matrix = matrixdepot("minij", 5)
     
     result = ffdiag(matrix)
-    is_diagonal = isdiag(result)
 
-    println("The result is a diagonal matrix: $is_diagonal")
+    @info(result[1])
+    # @info(matrix[1])
+    
+
+
+    # is_diagonal = isdiag(result)
+
+    # println("The result is a diagonal matrix: $is_diagonal")
 
 end
